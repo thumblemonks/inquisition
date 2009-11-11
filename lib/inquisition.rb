1b312c1be7fc9cc18507f51502d9bdd175e2c3c8
Alexander Rakoczy <scissorjammer@gmail.com>
2009-11-11 01:10:08 -0600

    Get rid of some duplication and properly store options in the class, for inheritance

--- lib/inquisition.rb
+++ lib/inquisition.rb
@@ -10,6 +10,16 @@ module Inquisition
     klass.extend(ClassMethods)
   end
 
+  def self.sanitize(value, allow)
+    if allow
+      value.split(allow.blank? ? " " : allow).map do |partial|
+        HTML5libSanitize.sanitize_html(partial)
+      end.join(allow)
+    else
+      HTML5libSanitize.sanitize_html(value)
+    end
+  end
+
   module ClassMethods
     def sanitize_attribute(*attributes)
       sanitize_attribute_reader(*attributes)
@@ -20,21 +30,17 @@ module Inquisition
       options = attributes.last.is_a?(::Hash) ? attributes.pop : {}
       if respond_to?(:cleansed_attr_readers)
         write_inheritable_attribute(:cleansed_attr_readers, cleansed_attr_readers.concat(attributes))
+        write_inheritable_attribute(:cleansed_attr_reader_options, cleansed_attr_reader_options.merge(options))
       else
         write_inheritable_attribute(:cleansed_attr_readers, attributes)
+        write_inheritable_attribute(:cleansed_attr_reader_options, options)
         class_inheritable_reader(:cleansed_attr_readers)
+        class_inheritable_reader(:cleansed_attr_reader_options)
 
         define_method(:read_attribute_with_cleansing) do |attribute|
           value = read_attribute_without_cleansing(attribute)
           if cleansed_attr_readers.include?(attribute.to_sym) && !value.blank?
-            if allow = options[:allow][attribute.to_sym]
-              value = value.split(allow.blank? ? " " : allow)
-              value = value.map do |partial|
-                HTML5libSanitize.sanitize_html(partial)
-              end.join(allow)
-            else
-              HTML5libSanitize.sanitize_html(value)
-            end
+            Inquisition.sanitize(value,cleansed_attr_reader_options[:allow][attribute.to_sym])
           else
             value
           end
@@ -49,22 +55,17 @@ module Inquisition
       options = attributes.last.is_a?(::Hash) ? attributes.pop : {}
       if respond_to?(:cleansed_attr_writers)
         write_inheritable_attribute(:cleansed_attr_writers, cleansed_attr_writers.concat(attributes))
+        write_inheritable_attribute(:cleansed_attr_writer_options, cleansed_attr_writer_options.merge(options))
       else
         write_inheritable_attribute(:cleansed_attr_writers, attributes)
+        write_inheritable_attribute(:cleansed_attr_writer_options, options)
         class_inheritable_reader(:cleansed_attr_writers)
+        class_inheritable_reader(:cleansed_attr_writer_options)
 
         define_method(:write_attribute_with_cleansing) do |attribute, value|
           if cleansed_attr_writers.include?(attribute.to_sym) && !value.blank?
-            if allow = options[:allow][attribute.to_sym]
-              value = value.split(allow.blank? ? " " : allow)
-              value = value.map do |partial|
-                HTML5libSanitize.sanitize_html(partial)
-              end.join(allow)
-            else
-              HTML5libSanitize.sanitize_html(value)
-            end
+            Inquisition.sanitize(value,cleansed_attr_writer_options[:allow][attribute.to_sym])
           end
-
           write_attribute_without_cleansing(attribute, value)
         end
         alias_method_chain :write_attribute, :cleansing
